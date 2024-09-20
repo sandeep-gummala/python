832cbc929cda150c83dd5a970b136b83c91152b6
Gummala Sagar Sandeep Kumar <23pa1a0580@vishnu.edu.in>
2024-09-20 11:11:01 +0530

    Create multiple_return.py

---
+++ week7/multiple_return.py
@@ -0,0 +1,5 @@
+def calculation(a,b):
+    return a+b,a-b,a*b
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+print('sum ,difference and product of two numbers are:',calculation(a,b))
