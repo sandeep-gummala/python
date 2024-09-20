9a8f8bd9812fa070c2111c36963f98e4d90ced7a
Gummala Sagar Sandeep Kumar <23pa1a0580@vishnu.edu.in>
2024-09-20 11:07:22 +0530

    Update calculations.py

--- week7/calculations.py
+++ week7/calculations.py
@@ -1,1 +1,6 @@
-
+def calculation(a,b,o):
+    return eval(f'{a} {o} {b}')
+a=int(input("enter a number:"))
+b=int(input("enter second number:"))
+o=input("enter the operator:")
+print(calculation(a,b,o))
